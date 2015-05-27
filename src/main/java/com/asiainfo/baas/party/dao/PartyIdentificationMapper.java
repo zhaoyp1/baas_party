f4ddeae651199fbecd5ecf2ce3611c2b8b280c59
Zhao YunPing <zhaoyp@asiainfo.com>
2015-05-27 16:04:50 +0800

    partyIdentifity   DAO层

--- src/main/java/com/asiainfo/baas/party/dao/PartyIdentificationMapper.java
+++ src/main/java/com/asiainfo/baas/party/dao/PartyIdentificationMapper.java
@@ -1,6 +1,17 @@
 package com.asiainfo.baas.party.dao;
 
-public class PartyIdentificationMapper {
+import java.util.List;
+
+import com.asiainfo.baas.party.bean.PartyIdentification;
+
+public interface PartyIdentificationMapper {
+	
+	
+	public List<PartyIdentification> getPartyIdentificationByPartyId(String parttyId);
+	
+	public void createPartyIdentification(PartyIdentification partyIdentification);
+	
+	public void updatePartyIdentification(PartyIdentification partyIdentification);
 	
 	
 
