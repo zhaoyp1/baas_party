6bb45520c5a39b5b68b325893c373bab96e53b23
Zhao YunPing <zhaoyp@asiainfo.com>
2015-05-27 16:37:26 +0800

    添加完善信息

--- src/main/java/com/asiainfo/baas/party/service/PartyService.java
+++ src/main/java/com/asiainfo/baas/party/service/PartyService.java
@@ -21,5 +21,23 @@ public interface PartyService {
 	 * @param loginName
 	 */
 	boolean checkLoginNameUniqueness(String loginName);
+	
+	/**
+	 * 修改个人订户信息
+	 * @param individual
+	 */
+	void modifyIndividual(Individual individual);
+
+	/**
+	 * 修改组织信息
+	 * @param organization
+	 */
+	void modifyOrganization(Organization organization);
+
+	/**
+	 * 保存证件信息
+	 * @param identification
+	 */
+	void saveIdentification(PartyIdentification identification);
 
 }
--- src/main/java/com/asiainfo/baas/party/service/impl/PartyServiceImpl.java
+++ src/main/java/com/asiainfo/baas/party/service/impl/PartyServiceImpl.java
@@ -1,20 +1,38 @@
 package com.asiainfo.baas.party.service.impl;
 
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.asiainfo.baas.party.bean.Individual;
+import com.asiainfo.baas.party.bean.Organization;
 import com.asiainfo.baas.party.bean.Party;
+import com.asiainfo.baas.party.bean.PartyIdentification;
+import com.asiainfo.baas.party.bean.PartyRole;
+import com.asiainfo.baas.party.dao.IndividualMapper;
+import com.asiainfo.baas.party.dao.OrganizationMapper;
+import com.asiainfo.baas.party.dao.PartyIdentificationMapper;
 import com.asiainfo.baas.party.dao.PartyMapper;
+import com.asiainfo.baas.party.dao.PartyRoleMapper;
 import com.asiainfo.baas.party.service.PartyService;
 import com.asiainfo.baas.party.util.CommonUtil;
+import com.asiainfo.baas.party.util.PartyConst;
 @Service
 public class PartyServiceImpl implements PartyService {
 
 		@Autowired
 		private PartyMapper partyDao;
+		@Autowired
+		private PartyRoleMapper partyRoleDao;
+		@Autowired
+		private IndividualMapper indivDao;
+		@Autowired
+		private OrganizationMapper organDao;
+		@Autowired
+		private PartyIdentificationMapper partyIdenDao;
 	
 		/**
 		 * 
@@ -23,10 +41,33 @@ public class PartyServiceImpl implements PartyService {
 		public void createParty(Party party) {
 			
 			String id=CommonUtil.getGenerateId();
-			
+			Date createDate = new Date();
 			party.setPartyId(id);
-			
+			party.setStatus(PartyConst.CommonStatus_1);
+			party.setCreateDate(createDate);
 			partyDao.addParty(party);
+		
+			//个人订户
+			if(PartyConst.partyTypeIndividual.equals(party.getPartyType())){
+				Individual individual = new Individual();
+				individual.setIndividualId(id);
+				individual.setCreateDate(createDate);
+				indivDao.createIndividual(individual);
+			}else if(PartyConst.partyTypeOrganization.equals(party.getPartyType())){
+				//组织订户
+				Organization organization = new Organization();
+				organization.setOrganizationId(id);
+				organization.setCreateDate(createDate);
+				organDao.addOrganization(organization);
+			}
+			String partyRoleId = CommonUtil.getGenerateId();
+			PartyRole partyRole = new PartyRole();
+			partyRole.setPartyRoleId(partyRoleId);
+			partyRole.setPartyId(id);
+			partyRole.setModifyDate(createDate);
+			partyRole.setCreateDate(createDate);
+			partyRole.setStatus(PartyConst.CommonStatus_1);
+			partyRoleDao.addPartyRole(partyRole);
 		}
 
 	/**
@@ -56,6 +97,29 @@ public class PartyServiceImpl implements PartyService {
 		}
 		return false;
 		
+	}
+	/**
+	 * 修改个人订户信息
+	 * @param individual
+	 */
+	public void modifyIndividual(Individual individual) {
+		indivDao.updateIndividual(individual);
+	}
+	/**
+	 * 修改组织信息
+	 * @param organization
+	 */
+	 
+	public void modifyOrganization(Organization organization) {
+		organDao.updateOrganization(organization);
+	}
+	
+	/**
+	 * 保存证件信息
+	 * @param identification
+	 */
+	public void saveIdentification(PartyIdentification partyIdentification) {
+		partyIdenDao.createPartyIdentification(partyIdentification);
 	}
 
 }
