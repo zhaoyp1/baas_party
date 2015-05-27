df0e2c6e646ee0246eaa8f2408eb1032f115f54e
liurl3 <liurl3@asiainfo.com>
2015-05-27 16:29:28 +0800

    添加常量

--- src/main/java/com/asiainfo/baas/party/util/CommonUtil.java
+++ src/main/java/com/asiainfo/baas/party/util/CommonUtil.java
@@ -2,10 +2,22 @@ package com.asiainfo.baas.party.util;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class CommonUtil {
 	
+	/**
+	 * 自动生成ID
+	 */
 	public static String getGenerateId(){
 		return UUID.randomUUID().toString();
+	}
+	/**
+	 * 校验证件信息合法性
+	 */
+	public static boolean validateIdentification(String identityCode,String identityType){
+		return true;
 	}
 
 }
